d74029422db33297ba67ebb495733b0df0684db5
Sippora04 <sippora.toppo95@gmail.com>
2020-06-02 16:09:52 +0530

    Modified: Winner or Tie

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,19 +1,41 @@
 echo "Welcome to FlipCoinSimulator Problem"
+echo
 heads=0
 tails=0
 echo "How many times to flipping a Coin: "
 read num
-for (( i=1;i<=$num ;i++ ))
+
+for (( i=1; i<=$num; i++ ))
+#while [[ $heads -lt $num ]] || [[ $tails -lt $num ]]
 do
         x=$(( $RANDOM%2 ))
         if [ $x -eq 0 ]
         then
                 heads=$(( $heads + 1 ))
+               if [ $heads -eq $num ]
+                  then
+                     break
+               fi
 
         else
                 tails=$(( $tails + 1 ))
-
+               if [ $tails -eq $num ]
+                  then
+                     break
+               fi
         fi
 done
 echo "Head appear: "$heads
 echo "Tail appear: "$tails
+
+if [ $heads -gt $tails ]
+   then
+      diff=$(($heads-$tails))
+      echo "Heads won by $diff"
+   elif [ $tails -gt $heads ]
+      then
+         diff=$(($tails-$heads))
+         echo "Tails won by $diff"
+   else
+      echo "Its a TIE"
+fi
