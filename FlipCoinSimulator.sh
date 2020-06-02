8806da819a4b944fd61910cda41be2bfb09d34c7
Sippora04 <sippora.toppo95@gmail.com>
2020-06-02 16:14:26 +0530

    Refactor the code

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,41 +1,55 @@
+#!/bin/bash -x
 echo "Welcome to FlipCoinSimulator Problem"
-echo
+
 heads=0
 tails=0
-echo "How many times to flipping a Coin: "
-read num
 
-for (( i=1; i<=$num; i++ ))
-#while [[ $heads -lt $num ]] || [[ $tails -lt $num ]]
+while [[ $heads -le 21 || $tails -le 21 ]]
 do
-        x=$(( $RANDOM%2 ))
-        if [ $x -eq 0 ]
-        then
-                heads=$(( $heads + 1 ))
-               if [ $heads -eq $num ]
-                  then
-                     break
-               fi
-
-        else
-                tails=$(( $tails + 1 ))
-               if [ $tails -eq $num ]
-                  then
-                     break
-               fi
-        fi
+	random=$(( $RANDOM%2 ))
+   if [[ $random == 0 ]]
+     	then
+			if [ $heads -lt 21 ]
+				then
+					heads=$(( $heads + 1 ))
+			else
+					break
+			fi
+   else
+			if [ $tails -lt 21 ]
+            then
+               tails=$(( $tails + 1 ))
+         else
+               break
+         fi
+	fi
 done
 echo "Head appear: "$heads
 echo "Tail appear: "$tails
 
-if [ $heads -gt $tails ]
-   then
-      diff=$(($heads-$tails))
-      echo "Heads won by $diff"
-   elif [ $tails -gt $heads ]
-      then
-         diff=$(($tails-$heads))
-         echo "Tails won by $diff"
-   else
-      echo "Its a TIE"
+if [[ $heads -eq $tails ]]
+	then
+		echo "Its a TIE"
+		diff=$(($heads-$tails))
+		while [[ $diff -le 2 ]]
+			do
+				random=$(( RANDOM%2 ))
+				if [[ $random -eq 0 ]]
+					then
+						heads=$(($heads+1))
+						diff=$(($diff+2))
+					else
+						tails=$(($tails+1))
+						diff=$(($diff+2))
+				fi
+		done
+		echo "Head: $heads Tail: $tails"
+		elif [ $heads -gt $tails ]
+			then
+				diff=$(($heads-$tails))
+      		echo "Heads won by $diff"
+				elif [ $tails -gt $heads ]
+					then
+						diff=$(($tails-$heads))
+						echo "Tails won by $diff"
 fi
