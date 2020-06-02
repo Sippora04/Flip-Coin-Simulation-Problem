d85775368d4de3ffa0cddfbae682dc4c5836ad3a
Sippora04 <sippora.toppo95@gmail.com>
2020-06-02 15:55:44 +0530

    MODIFY: printing number of H & T

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,1 +1,19 @@
-echo "Welcome to Flip Coin Simulation"
+echo "Welcome to FlipCoinSimulator Problem"
+heads=0
+tails=0
+echo "How many times to flipping a Coin: "
+read num
+for (( i=1;i<=$num ;i++ ))
+do
+        x=$(( $RANDOM%2 ))
+        if [ $x -eq 0 ]
+        then
+                heads=$(( $heads + 1 ))
+
+        else
+                tails=$(( $tails + 1 ))
+
+        fi
+done
+echo "Head appear: "$heads
+echo "Tail appear: "$tails
